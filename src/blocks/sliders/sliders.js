a604f5ae095de651d7e8c9b5823fa604c642b1a2
NikitaLem <saintsuigintoufag@gmail.com>
2018-02-19 22:12:35 +0600

    green slider whith value grade added

--- src/blocks/sliders/sliders.js
+++ src/blocks/sliders/sliders.js
@@ -1,9 +1,12 @@
-var slider = document.querySelector('.sliders');
+/*JS code for sliders with popup*/
+var sliderPopup = document.querySelectorAll('.sliders_popup');
 var popup = document.querySelector('.sliders__popup');
 
-slider.addEventListener('mousemove', popupGo, false);
-slider.addEventListener('mousedown', popupShow, false);
-slider.addEventListener('mouseup', popupHide, false);
+for(var i =0; i<sliderPopup.length; i++) {
+    sliderPopup[i].addEventListener('mousemove', popupGo, false);
+    sliderPopup[i].addEventListener('mousedown', popupShow, false);
+    sliderPopup[i].addEventListener('mouseup', popupHide, false);
+}
 
 function popupGo(e) {
     popup.style.left = e.clientX -  popup.getBoundingClientRect().width/2 + 'px';
@@ -19,3 +22,10 @@ function popupShow() {
 function popupHide() {
     popup.classList.remove('sliders__popup_show');
 }
+/*JS code for sliders with track*/
+function sliderTrack() {
+    var val = $('.sliders_track').val();
+    $('.sliders_track').css({'background':'-webkit-linear-gradient(left, #4eb7a8 0%, #4eb7a8 '+val+'%, #e5e5e5 '+val+'% ,#e5e5e5 100%)'});
+}
+
+document.querySelector('.sliders_track').addEventListener('input', sliderTrack, false);
