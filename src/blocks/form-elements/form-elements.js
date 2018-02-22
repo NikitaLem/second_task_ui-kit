4c1c1bbd0879a5db473da4a546217d89cbc43797
NikitaLem <saintsuigintoufag@gmail.com>
2018-02-22 23:53:02 +0600

    form element added 1

---
+++ src/blocks/form-elements/form-elements.js
@@ -0,0 +1,21 @@
+var inputs = document.querySelectorAll('.form__input');
+var alerts = document.querySelectorAll('.form__alert');
+
+for(var i=0; i<inputs.length; i++) {
+    inputs[i].addEventListener('input', helper, false);
+}
+
+function helper() {
+    for(var i=0; i<inputs.length; i++) {
+        if(inputs[i].value=='') {
+            alerts[i].innerHTML='ERROR';
+            alerts[i].classList.remove('form__alert_green');
+            alerts[i].classList.add('form__alert_red');
+        }
+        if(inputs[i].value!=='') {
+            alerts[i].innerHTML='THANKS!';
+            alerts[i].classList.remove('form__alert_red');
+            alerts[i].classList.add('form__alert_green');
+        }
+    }
+}
--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,7 @@ import '../blocks/prc-charts/pie-charts.js';
 import '../blocks/prc-charts/prc-charts.js';
 import '../blocks/sliders/sliders.js';
 import '../blocks/stages/stages.js';
+import '../blocks/form-elements/form-elements.js';
 
 var html = require('../blocks/index.pug');
 const css = require('../blocks/index.styl');
