8e5745f9949401b62ba863d1ddbc831305031672
NikitaLem <saintsuigintoufag@gmail.com>
2018-03-03 00:50:16 +0600

    now news and events in mixins

--- src/js/index.js
+++ src/js/index.js
@@ -5,6 +5,7 @@ import '../blocks/prc-charts/prc-charts.js';
 import '../blocks/sliders/sliders.js';
 import '../blocks/stages/stages.js';
 import '../blocks/form-elements/form-elements.js';
+import '../blocks/location/location.js';
 
 var html = require('../blocks/index.pug');
 const css = require('../blocks/index.styl');
