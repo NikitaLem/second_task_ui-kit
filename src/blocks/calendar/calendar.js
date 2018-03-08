ee39dea6ac5ea18c20055ed3d081faa8bac9a58a
NikitaLem <saintsuigintoufag@gmail.com>
2018-03-08 23:04:06 +0600

    calendar scrpits added
    still have to make Date scripts

--- src/blocks/calendar/calendar.js
+++ src/blocks/calendar/calendar.js
@@ -1,5 +1,17 @@
 var day = document.querySelectorAll('.calendar__list-item');
 
 for(var i = 0; i < day.length; i++) {
-    if()
+    if(day[i].classList.contains('calendar__list-item_empty') == false) {
+        day[i].addEventListener('click', getDay, false);
+    }
+}
+
+function getDay(e) {
+    for(var j = 0; j < day.length; j++) {
+        day[j].classList.remove('calendar__list-item_active');
+    }
+    if(e.target.classList.contains('calendar__list-item_active') == false) {
+        e.target.classList.add('calendar__list-item_active');
+    }
+    document.querySelector('.calendar__date').innerHTML = e.target.innerHTML;
 }
