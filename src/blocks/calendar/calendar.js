b9413603385c680d8c497e1e3524154db13cfae0
NikitaLem <saintsuigintoufag@gmail.com>
2018-03-10 20:15:57 +0600

    some calendar changes

--- src/blocks/calendar/calendar.js
+++ src/blocks/calendar/calendar.js
@@ -1,10 +1,16 @@
 var day = document.querySelectorAll('.calendar__list-item');
+var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+var date = new Date();
+var k = date.getMonth();
 
 for(var i = 0; i < day.length; i++) {
     if(day[i].classList.contains('calendar__list-item_empty') == false) {
         day[i].addEventListener('click', getDay, false);
     }
 }
+
+document.querySelector('.calendar__button_left').addEventListener('click', prevMonth, false);
+document.querySelector('.calendar__button_right').addEventListener('click', nextMonth, false);
 
 function getDay(e) {
     for(var j = 0; j < day.length; j++) {
@@ -14,4 +20,20 @@ function getDay(e) {
         e.target.classList.add('calendar__list-item_active');
     }
     document.querySelector('.calendar__date').innerHTML = e.target.innerHTML;
+}
+
+function nextMonth() {
+    k++
+    if(k>11) {
+        k = 0;
+    }
+    document.querySelector('.calendar__month').innerHTML = months[k];
+}
+
+function prevMonth() {
+    k--;
+    if(k<0) {
+        k = 11;
+    }
+    document.querySelector('.calendar__month').innerHTML = months[k];
 }
