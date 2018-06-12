5dc035364c90d5f167aa2b6006062163fdee1148
NikitaLem <saintsuigintoufag@gmail.com>
2018-06-13 03:14:27 +0600

    htm-refactoring-inprogress

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-# first_task_cv
-for Fullstack Development
+# second_task_ui-kit
+Second task for Fullstack Development. Ñreate a set of standard web page elements.
 https://nikitalem.github.io/second_task_ui-kit/
+
+Download, use "npm install" for install node_modules, type "npm run dev" for run project on local server.
--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,3 @@
-import greet from './greeter.js';
 import '../blocks/buttons/buttons.js';
 import '../blocks/prc-charts/pie-charts.js';
 import '../blocks/prc-charts/prc-charts.js';
@@ -10,6 +9,3 @@ import '../blocks/messaging/messaging.js';
 
 var html = require('../blocks/index.pug');
 const css = require('../blocks/index.styl');
-
-console.log("I'm the enty point");
-greet();
