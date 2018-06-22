dcc5347ad179f16138d444339de3c5c54ca224fc
NikitaLem <saintsuigintoufag@gmail.com>
2018-06-23 02:37:39 +0600

    refactoring-inprogress/news and events done

--- src/blocks/messaging/messaging.js
+++ src/blocks/messaging/messaging.js
@@ -1,11 +1,11 @@
 document.querySelector('.messaging__button').addEventListener('click', send, false);
 
 function send() {
-    var sendText = document.querySelector('.messaging__message_give').innerHTML;
+    var sendText = document.querySelector('.messaging__text-box_sent').innerHTML;
     var newMessage = document.createElement("DIV");
-    newMessage.classList.add("messaging__message", "messaging__message_get");
+    newMessage.classList.add("messaging__text-box", "messaging__text-box_received");
     newMessage.innerHTML = sendText;
     document.querySelector('.messaging__chat-box').appendChild(newMessage);
-    sendText = 'Hurr!';
+    sendText = '';
     document.querySelector('.messaging__chat-box').scrollTop = document.querySelector('.messaging__chat-box').scrollHeight;
 }
